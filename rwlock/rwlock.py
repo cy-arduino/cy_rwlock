3431a91aeb0e25549ffc31ffbe104dc6f2a72129
ChihYing <chihying.phone@gmail.com>
2019-11-29 16:36:13 +0800

    update

--- rwlock/rwlock.py
+++ rwlock/rwlock.py
@@ -2,27 +2,14 @@ from threading import Lock
 # from contextlib import contextmanager
 
 
-# RwLock: Reader-Writer lock
-#
-# We can simply protect a shared resource by a lock. But the performance is not
-# good because each reader should done one-by-one.
-#
-# A Reader-Writer lock can improve the performance by
-#   1. Let readers running simultaneously
-#   2. Exclude "multiple readers" and each writer
-#
-# By the way, a writer should wait until all reader done.
-# In a frequently read situation, a reader after the writer can also increase
-# the read count, let read count never decrease to 0. This will starve writer.
-# So RwLock provide a flag "write_first" to block the newer read until the
-# writer finish it's work.
 class RwLock:
     def __init__(self, write_first=True):
+        # prevent writer starvation. block new read when someone want to write.
         self._write_first = write_first
 
         self._write_lock = Lock()
         if self._write_first:
-            self._read_lock = Lock()  # prevent writer starvation. block new read when someone want to write.
+            self._read_lock = Lock()
         self._read_cnt_lock = Lock()
         self._read_cnt = 0
 
